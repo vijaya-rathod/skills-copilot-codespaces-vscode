05652eea9a2fcb799a5b6eebd87f07eb7aafd670
vijaya-rathod <119282682+vijaya-rathod@users.noreply.github.com>
2024-12-26 07:41:29 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+//create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function(req, res){
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname
