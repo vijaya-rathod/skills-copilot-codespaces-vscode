d07bf7518fbd1a1da0f34dbe18c1bd4098fbe3d6
vijaya-rathod <119282682+vijaya-rathod@users.noreply.github.com>
2024-12-26 07:43:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,4 +5,15 @@ var url = require('url');
 
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
-    var filename = "." + q.pathname
+    var filename = "." + q.pathname + ".html";  
+    fs.readFile(filename, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    }); 
+}
+).listen(8080);
