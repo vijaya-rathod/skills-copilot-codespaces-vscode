ee17893615017586dd0252e3ee43425b16fcd1f1
vijaya-rathod <119282682+vijaya-rathod@users.noreply.github.com>
2024-12-26 07:46:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,41 @@
 //create web server
-var http = require('http');
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var url = require('url');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-http.createServer(function(req, res){
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname + ".html";  
-    fs.readFile(filename, function(err, data){
-        if(err){
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
+app.use(express.static('public'));
+app.get('/comments', function (req, res) {
+    console.log('GET request received at /comments');
+    fs.readFile(__dirname + '/public/comments.json', 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
+});
+
+app.post('/comments', urlencodedParser, function (req, res) {
+    console.log('POST request received at /comments');
+    var newComment = req.body;
+    console.log(newComment);
+    fs.readFile(__dirname + '/public/comments.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    }); 
-}
-).listen(8080);
+        else {
+            var comments = JSON.parse(data);
+            comments.push(newComment);
+            fs.writeFile(__dirname + '/public/comments.json', JSON.stringify(comments), function (err) {
+                console.log(err);
+            });
+        }
+    });
+    res.send('Comment added');
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server listening at http://%s:%s", host, port);
+});
+//end of comments.js
